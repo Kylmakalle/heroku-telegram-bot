4fedb7cdb83ef5d96f42857a6bc3b5011beddcd9
Pyoss <yisgit@gmail.com>
2017-07-19 23:30:49 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,18 +1,13 @@
 # -*- coding: utf-8 -*-
-import redis
 import os
 import telebot
-# import some_api_lib
-# import ...
+import requests
+from telebot import types
 
 # Example of your code beginning
 #           Config vars
 token = os.environ['TELEGRAM_TOKEN']
-some_api_token = os.environ['SOME_API_TOKEN']
-#             ...
 
-# If you use redis, install this add-on https://elements.heroku.com/addons/heroku-redis
-r = redis.from_url(os.environ.get("REDIS_URL"))
 
 #       Your bot code below
 
