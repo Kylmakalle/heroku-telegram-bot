dc2544944596713dbb5056c6706338aa1135dc2f
Pyoss <yisgit@gmail.com>
2018-04-19 08:28:43 +0300

    Add files via upload

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
 import os
 
 token = os.environ['TELEGRAM_TOKEN']
-admin_id = os.environ['ADMIN_ID']
-channel_id = os.environ['CHANNEL_ID']
+ratid = 'AgADAgADhKgxG3FBOUhDPVtu5UzB6tSBtw0ABGSIMIw_REmSqeICAAEC'
