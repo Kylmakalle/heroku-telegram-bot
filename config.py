e1aebf29b3fa653e546e0379369fc3b5cad54e75
Pyoss <yisgit@gmail.com>
2018-04-20 22:00:32 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
-token = "591794542:AAFZZFxYtTr2_4g86W4fw3b1zQKtXrBDIxo"
-admin_id = 197216910
-channel_id = '@test_kiriya'
+token = os.environ['TELEGRAM_TOKEN']
+admin_id = os.environ['ADMIN_ID']
+channel_id = os.environ['CHANNEL_ID']
