531271e982ac55eed97b8e10e27051b1b97b0fd8
Pyoss <yisgit@gmail.com>
2018-04-21 13:49:59 +0300

    Update bot_handlers.py

--- bot_handlers.py
+++ bot_handlers.py
@@ -6,16 +6,28 @@ bot = telebot.TeleBot(config.token)
 
 
 def send_message(chat_id, text, reply_markup=None):
-    return bot.send_message(chat_id, text, reply_markup=reply_markup)
+    try:
+        return bot.send_message(chat_id, text, reply_markup=reply_markup)
+    except:
+        pass
 
 
 def send_photo(chat_id, photo, caption=None, reply_markup=None):
-    return bot.send_photo(chat_id=chat_id, photo=photo, caption=caption, reply_markup=reply_markup)
+    try:
+        return bot.send_photo(chat_id=chat_id, photo=photo, caption=caption, reply_markup=reply_markup)
+    except:
+        pass
 
 
 def edit_message(chat_id, message_id, text, reply_markup=None):
-    bot.edit_message_text(message_id=message_id, chat_id=chat_id, text=text, reply_markup=reply_markup)
+    try:
+        bot.edit_message_text(message_id=message_id, chat_id=chat_id, text=text, reply_markup=reply_markup)
+    except:
+        pass
 
 
 def send_media_group(chat_id, media_group):
-    bot.send_media_group(chat_id, media=media_group)
+    try:
+        bot.send_media_group(chat_id, media=media_group)
+    except:
+        pass
